d4a4fa81e5349110ae869e4c2c588582cd2daa9d
nozymech <sjyang.pt@gmail.com>
2022-05-07 21:04:11 +0800

    Complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+let result = []
 
+function repeatString (string,num) {
+result = ""
+if (num>=0) {
+    for (i=0;i<num;i++){
+        result += string
+    }
+    return result
+}
+else {
+    return 'ERROR'
+}
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
