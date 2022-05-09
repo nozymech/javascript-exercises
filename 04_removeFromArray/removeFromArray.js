c3e9e4e3a0f150f0cae3ee4fdeaf11a087e86b8c
nozymech <sjyang.pt@gmail.com>
2022-05-09 23:18:07 +0800

    complete 04-removefromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+function removeFromArray(array,remove1,remove2,remove3,remove4) {
+let newArray = array.filter(array => array !== remove1 && array !== remove2 && array !== remove3 && array !== remove4);
+return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
