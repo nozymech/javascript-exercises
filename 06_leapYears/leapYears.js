5fe57048b5c9e1719f16b542023ff19f14642b96
nozymech <sjyang.pt@gmail.com>
2022-05-11 01:08:02 +0800

    complete 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+function leapYears(year)  {
+if (year%4 ==0 && (year%100!=0 || year%400==0)){
+    return true
+}
+else{
+    return false
+}
 };
 
 // Do not edit below this line
