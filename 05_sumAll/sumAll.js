3d9ef778469017918d5e0013138f035335f6ea49
nozymech <sjyang.pt@gmail.com>
2022-05-11 00:45:13 +0800

    complete 05-symAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+function sumAll(a,b){
+let sum =0
+if (a<0 || b<0){
+    return 'ERROR'
+}
+else if (typeof a !== 'number' || typeof b !== 'number'){
+    return 'ERROR'
+}
 
-};
+else if(a<b){
+   for(let i=a; i<b+1; i++ ){
+    sum = sum + i
+} return sum; 
+}
+else if(a>b){
+    for(let i=b; i<a+1; i++ ){
+        sum = sum + i
+    } return sum; 
+}
+
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
