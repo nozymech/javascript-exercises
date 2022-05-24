b613cb26d7d06b299581214f3051cf39b310a9af
nozymech <sjyang.pt@gmail.com>
2022-05-24 23:28:03 +0800

    complete 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b ;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0 ;
+  for(i=0 ; i<array.length ; i++){
+    sum = sum + array[i] ;
+  }
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1 ;
+  for(i=0 ; i<array.length; i++){
+    result = result*array[i];
+  }
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+  for(i=0; i<b; i++){
+    result = result*a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for(i=1; i<a+1 ; i++){
+    result = result*i
+  }
+  return result;
 };
 
 // Do not edit below this line
