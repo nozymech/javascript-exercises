4f4aa2723ede5cc85aefd1dbe565ed17e06fc5bf
nozymech <sjyang.pt@gmail.com>
2022-05-25 23:33:52 +0800

    complete 12-findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (let j=0; j<people.length; j++ ){
+        if (people[j].yearOfDeath == undefined){
+            const d = new Date();
+            people[j].yearOfDeath = d.getFullYear();
+        }
+    }
+    people[0].livingTime = people[0].yearOfDeath - people[0].yearOfBirth ;
+    people[1].livingTime = people[1].yearOfDeath - people[1].yearOfBirth ;
+    people[2].livingTime = people[2].yearOfDeath - people[2].yearOfBirth ;
+        let oldestMan = people[0];
+        for (let i=1 ; i< people.length ; i++) {
+            if(people[i].livingTime > oldestMan.livingTime) {
+                oldestMan = people[i]; 
+            }
+        }
+  return oldestMan;
 };
 
 // Do not edit below this line
