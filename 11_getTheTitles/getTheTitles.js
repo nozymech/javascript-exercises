0811afa2652b6415303fc95cda27a0f0e679258c
nozymech <sjyang.pt@gmail.com>
2022-05-25 21:56:47 +0800

    complete 11-getTheTitle

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+  
 const getTheTitles = function() {
-
+let result = [books[0].title ,books[1].title ];
+return result;
 };
 
 // Do not edit below this line
