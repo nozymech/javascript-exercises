273499246a86034a0d211588cae05627c40ebe1c
nozymech <sjyang.pt@gmail.com>
2022-05-25 12:13:15 +0800

    complete 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let result = 0;
+    if (num <= 2 && num >0){
+        return 1;
+    }else if(num >2){
+        result = fibonacci(num-1) + fibonacci(num-2);
+    }else{
+        return "OOPS"
+    }
+return result
 };
 
 // Do not edit below this line
