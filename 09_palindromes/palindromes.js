be454822b156ad84d2d1acdc184fdc1ce89db103
nozymech <sjyang.pt@gmail.com>
2022-05-25 11:11:32 +0800

    complete 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
-};
+const palindromes = function (s) {
+    let multiQuote = /,/gi;
+    let multiSpace = / /gi;
+    let words = s.replace("!","").replace(".","").replace(multiSpace,"").replace(multiQuote,"").toLowerCase()
+    let reverse = words.split("").reverse().join("");
+    
+    if (words === reverse){
+        return true;
+    }
+    else {
+        return false;
+    }
+    };
+    
 
 // Do not edit below this line
 module.exports = palindromes;
